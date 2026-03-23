69a3c607559042ff7f0cb8796ef1d54f51bc85fe
ChocoFanta-01 <ljh970512@naver.com>
2026-03-23 23:21:33 +0900

    테스크 정리 앱 기초 세팅

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { appContainer } from "./App.css"
+
+
+function App() {
+
+
+  return (
+    <div className={appContainer}>
+      <div>
+
+      </div>
+      <div>
+        <button>
+          이 게시판 삭제하기
+        </button>
+        <button>
+
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/hooks/redux.ts
@@ -0,0 +1,7 @@
+import type { RootState,AppDispatch } from "../store";
+import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
+
+
